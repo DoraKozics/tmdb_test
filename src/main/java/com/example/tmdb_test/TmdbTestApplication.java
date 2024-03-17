030d6977d28f9e190d0d068a2ea92b29069cd57d
kozic <kozicsdora@gmail.com>
2024-03-17 15:30:59 +0100

    Initial commit

---
+++ src/main/java/com/example/tmdb_test/TmdbTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmdb_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tmdb_test/TmdbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmdb_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
